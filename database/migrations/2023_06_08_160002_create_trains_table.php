8aeab170b79feeeef69773405b8ee2283894570d
marcocnc <marcocancelliere56@gmail.com>
2023-06-08 18:08:09 +0200

    created tables data in migration file

--- database/migrations/2023_06_08_160002_create_trains_table.php
+++ database/migrations/2023_06_08_160002_create_trains_table.php
@@ -15,6 +15,16 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company', 30);
+            $table->string('slug', 40)->unique();
+            $table->string('departure_station', 50);
+            $table->string('arrival_station', 50);
+            $table->string('departure_time', 5);
+            $table->string('arrival_time', 5);
+            $table->smallInteger('code')->unsigned()->unique();
+            $table->tinylInteger('carriages')->unsigned();
+            $table->boolean('in_time');
+            $table->boolean('cancelled');
             $table->timestamps();
         });
     }
